135bc6d95ebf6efab57081d1f99f5c8d87f7a2b2
Gustavo Mendes <73292746+gutinha@users.noreply.github.com>
2021-06-15 11:33:14 -0300

    First upload

---
+++ legendas.js
@@ -0,0 +1,13 @@
+exports.configg = 'prefix, setprefix,.';
+
+exports.diversao = '8ball, avatar, baka, beijar, call, corrida, drexzin, fake, feed, fotos, hug, kiss, meow, pat, poke, pokegif, random, ratewaifu, say, tapa, tickle, uptime, waifu.';
+
+exports.minecraft = 'mcicon, mcserver, mcskin.';
+
+exports.moderacao = 'ban, kick, mute, lock, unlock, unmute, unban.';
+
+exports.nsfw = 'anal, ero, gif, holoero, keel, keelgif, les, lewd, solo, tits, trap.';
+
+exports.tankionline = 'tanki, toinfo.';
+
+exports.utilitarios = 'anunciar, botinfo, check, clear, donate, limpar, ping, reportar, servericon, serverinfo, skin, status, userinfo.';
